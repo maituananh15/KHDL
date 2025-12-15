49ccd9b03d57c3a563cb7f570498a7d14a01d2da
DEv2405LM-PHP <taomalai987@gmail.com>
2025-12-15 19:44:21 +0700

    KHDL

--- config/database.js
+++ config/database.js
@@ -1,17 +1,18 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    if (!process.env.MONGODB_URI) {
-      throw new Error("MONGODB_URI is not defined");
-    }
-
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://khdl.nu8iipv.mongodb.net/khdl', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error("MongoDB connection error:", error.message);
+    console.error(`Error: ${error.message}`);
     process.exit(1);
   }
 };
 
 module.exports = connectDB;
+
